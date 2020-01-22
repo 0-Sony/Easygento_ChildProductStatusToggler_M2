bf6f4eedd5b6efb0ca4396849c2379c590a60909
Phuong LÊ <phuong.le@menincode.com>
2020-01-22 17:24:28 +0000

    Refacto - Use Product Repository Interface

--- Observer/AfterProductSave.php
+++ Observer/AfterProductSave.php
@@ -7,11 +7,17 @@
 
 namespace Easygento\ChildProductStatusToggler\Observer;
 
+use Exception;
+use Magento\Catalog\Api\Data\ProductInterface;
+use Magento\Catalog\Api\ProductRepositoryInterface;
 use Magento\Catalog\Model\Product;
+use Magento\Catalog\Model\Product\Attribute\Source\Status;
+use Magento\ConfigurableProduct\Model\Product\Type\Configurable;
 use Magento\Framework\Event\Observer;
 use Magento\Framework\Event\ObserverInterface;
-use Magento\ConfigurableProduct\Model\Product\Type\Configurable;
-use Magento\Catalog\Model\Product\Attribute\Source\Status;
+use Magento\Framework\Exception\CouldNotSaveException;
+use Magento\Framework\Exception\InputException;
+use Magento\Framework\Exception\StateException;
 use Magento\Framework\Message\ManagerInterface;
 
 class AfterProductSave implements ObserverInterface
@@ -20,27 +26,55 @@ class AfterProductSave implements ObserverInterface
      * @var ManagerInterface
      */
     private $messageManager;
+    /**
+     * @var ProductRepositoryInterface
+     */
+    private $productRepository;
 
+    /**
+     * AfterProductSave constructor.
+     * @param ManagerInterface $messageManager
+     * @param ProductRepositoryInterface $productRepository
+     */
     public function __construct(
-        ManagerInterface $messageManager
+        ManagerInterface $messageManager,
+        ProductRepositoryInterface $productRepository
     ) {
         $this->messageManager = $messageManager;
+        $this->productRepository = $productRepository;
     }
 
     /**
      * @inheritDoc
+     * @throws Exception
      */
     public function execute(Observer $observer)
     {
         /** @var Product $product */
         $product = $observer->getData('product');
 
-
         if ($product->getTypeId() == Configurable::TYPE_CODE) {
             $_children = $product->getTypeInstance()->getUsedProducts($product);
-            foreach ($_children as $child) {
-                $child->setStatus($product->getStatus());
-                $child->save();
+            /** @var Product $child */
+            foreach ($product->getStoreIds() as $storeId) {
+                foreach ($_children as $child) {
+
+                    /** @var ProductInterface $childProduct */
+                    $childProduct = $this->productRepository->getById($child->getId());
+                    $childProduct->setStatus($product->getStatus());
+                    $childProduct->setStoreId($storeId);
+
+                    try {
+                        $this->productRepository->save($childProduct);
+
+                    } catch (CouldNotSaveException $e) {
+                        $this->messageManager->addErrorMessage($e->getMessage());
+                    } catch (InputException $e) {
+                        $this->messageManager->addErrorMessage($e->getMessage());
+                    } catch (StateException $e) {
+                        $this->messageManager->addErrorMessage($e->getMessage());
+                    }
+                }
             }
             $message = $product->getStatus() == Status::STATUS_DISABLED
                 ? __('The child products associated have been disabled')
--- Observer/ProductAttributeUpdateBefore.php
+++ Observer/ProductAttributeUpdateBefore.php
@@ -7,17 +7,22 @@
 
 namespace Easygento\ChildProductStatusToggler\Observer;
 
+use Magento\Catalog\Api\Data\ProductInterface;
+use Magento\Catalog\Api\ProductRepositoryInterface;
 use Magento\Catalog\Model\Product;
 use Magento\Catalog\Model\Product\Attribute\Source\Status;
 use Magento\ConfigurableProduct\Model\Product\Type\Configurable;
 use Magento\Framework\Event\Observer;
 use Magento\Framework\Event\ObserverInterface;
+use Magento\Framework\Exception\CouldNotSaveException;
+use Magento\Framework\Exception\InputException;
+use Magento\Framework\Exception\StateException;
 use Magento\Framework\Message\ManagerInterface;
 
 class ProductAttributeUpdateBefore implements ObserverInterface
 {
     /**
-     * @var \Magento\Catalog\Api\ProductRepositoryInterface
+     * @var ProductRepositoryInterface
      */
     private $productRepository;
     /**
@@ -27,11 +32,11 @@ class ProductAttributeUpdateBefore implements ObserverInterface
 
     /**
      * ProductAttributeUpdateBefore constructor.
-     * @param \Magento\Catalog\Api\ProductRepositoryInterface $productRepository
+     * @param ProductRepositoryInterface $productRepository
      * @param ManagerInterface $messageManager
      */
     public function __construct(
-        \Magento\Catalog\Api\ProductRepositoryInterface $productRepository,
+        ProductRepositoryInterface $productRepository,
         ManagerInterface $messageManager
     ) {
         $this->productRepository = $productRepository;
@@ -40,24 +45,40 @@ class ProductAttributeUpdateBefore implements ObserverInterface
 
     /**
      * @inheritDoc
+     * @throws \Magento\Framework\Exception\NoSuchEntityException
      */
     public function execute(Observer $observer)
     {
         $productIds = $observer->getData('product_ids');
         $attributesData = $observer->getData('attributes_data');
         $statusIsUpdated = array_key_exists('status', $attributesData);
-
         if ($statusIsUpdated) {
             foreach ($productIds as $id) {
+                /** @var Product $product */
                 $product = $this->productRepository->getById($id);
+
                 if ($product->getTypeId() == Configurable::TYPE_CODE) {
                     $_children = $product->getTypeInstance()->getUsedProducts($product);
                     /** @var Product $child */
                     foreach ($_children as $child) {
-                        $child->setStatus($attributesData['status']);
-                        $child->save();
+
+                        /** @var ProductInterface $childProduct */
+                        $childProduct = $this->productRepository->getById($child->getId());
+                        $childProduct->setStatus($attributesData['status']);
+
+                        try {
+                            $this->productRepository->save($childProduct);
+
+                        } catch (CouldNotSaveException $e) {
+                            $this->messageManager->addErrorMessage($e->getMessage());
+                        } catch (InputException $e) {
+                            $this->messageManager->addErrorMessage($e->getMessage());
+                        } catch (StateException $e) {
+                            $this->messageManager->addErrorMessage($e->getMessage());
+                        }
                     }
-                    $message = $product->getStatus() == Status::STATUS_DISABLED
+
+                    $message = $attributesData['status'] == Status::STATUS_DISABLED
                         ? __('The child products associated have been disabled')
                         : __('The child products associated have been enabled');
 
